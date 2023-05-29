d233e8424366d3f33a772b10946b0eb01390d739
nuric <nuric@users.noreply.github.com>
2023-05-29 10:56:21 +0000

    Add extra dist set test cases

--- collection/distset_test.go
+++ collection/distset_test.go
@@ -33,6 +33,25 @@ func TestDistSet_Add_Duplicate(t *testing.T) {
 	ds.Add(&DistSetElem{distance: 0.2, id: "3"})
 	ds.Add(&DistSetElem{distance: 0.4, id: "3"})
 	assert.Equal(t, 3, ds.Len())
+	ds.Sort()
+	wantOrder := []string{"3", "2", "1"}
+	for i, elem := range ds.items {
+		assert.Equal(t, wantOrder[i], elem.id)
+	}
+}
+
+func TestDistSet_KeepFirstK(t *testing.T) {
+	ds := NewDistSet(2)
+	ds.Add(&DistSetElem{distance: 0.5, id: "2"})
+	ds.Add(&DistSetElem{distance: 1.0, id: "1"})
+	ds.Add(&DistSetElem{distance: 0.2, id: "3"})
+	ds.Sort()
+	ds.KeepFirstK(2)
+	assert.Equal(t, 2, ds.Len())
+	wantOrder := []string{"3", "2"}
+	for i, elem := range ds.items {
+		assert.Equal(t, wantOrder[i], elem.id)
+	}
 }
 
 func TestDistSet_Pop_Remove(t *testing.T) {
