a08cc348ecfed595765fa54451dc69d68631ee8a
nuric <nuric@users.noreply.github.com>
2023-07-09 10:04:23 +0000

    bbolt kv as backend

--- shard/shard.go
+++ shard/shard.go
@@ -5,27 +5,34 @@ import (
 	"path/filepath"
 	"sync"
 
-	"github.com/dgraph-io/badger/v4"
 	"github.com/google/uuid"
 	"github.com/rs/zerolog/log"
 	"github.com/schollz/progressbar/v3"
 	"github.com/semafind/semadb/models"
+	"go.etcd.io/bbolt"
 )
 
 type Shard struct {
-	db         *badger.DB
+	db         *bbolt.DB
 	collection models.Collection
 	closeCount int
 	closeLock  sync.Mutex
 }
 
 func NewShard(shardDir string, collection models.Collection) (*Shard, error) {
-	dbDir := filepath.Join(shardDir, "db")
-	opts := badger.DefaultOptions(dbDir).WithLogger(nil)
-	db, err := badger.Open(opts)
+	// dbDir := filepath.Join(shardDir, "db")
+	// opts := badger.DefaultOptions(dbDir).WithLogger(nil)
+	db, err := bbolt.Open(filepath.Join(shardDir, "db"), 0666, nil)
 	if err != nil {
 		return nil, fmt.Errorf("could not open shard db: %w", err)
 	}
+	db.Update(func(tx *bbolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte("points"))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		return nil
+	})
 	return &Shard{
 		db:         db,
 		collection: collection,
@@ -52,25 +59,25 @@ func (s *Shard) Close() error {
 
 // ---------------------------
 
-func (s *Shard) insertPoint(txn *badger.Txn, startPoint ShardPoint, point ShardPoint) error {
+func (s *Shard) insertPoint(b *bbolt.Bucket, startPoint ShardPoint, point ShardPoint) error {
 	// ---------------------------
-	_, visitedSet, err := s.greedySearch(txn, startPoint, point.Vector, 1, s.collection.Parameters.SearchSize)
+	_, visitedSet, err := s.greedySearch(b, startPoint, point.Vector, 1, s.collection.Parameters.SearchSize)
 	if err != nil {
 		return fmt.Errorf("could not greedy search: %w", err)
 	}
 	// ---------------------------
-	s.robustPrune(txn, &point, visitedSet, s.collection.Parameters.Alpha, s.collection.Parameters.DegreeBound)
+	s.robustPrune(&point, visitedSet, s.collection.Parameters.Alpha, s.collection.Parameters.DegreeBound)
 	// ---------------------------
 	// Add the bi-directional edges
 	for _, nId := range point.Edges {
-		n, err := s.getPoint(txn, nId)
+		n, err := s.getPoint(b, nId)
 		if err != nil {
 			return fmt.Errorf("could not get neighbour point: %w", err)
 		}
 		if len(n.Edges)+1 > s.collection.Parameters.DegreeBound {
 			// ---------------------------
 			// We need to prune the neighbour as well to keep the degree bound
-			nn, err := s.getPointNeighbours(txn, n)
+			nn, err := s.getPointNeighbours(b, n)
 			if err != nil {
 				return fmt.Errorf("could not get neighbour neighbours: %w", err)
 			}
@@ -78,18 +85,18 @@ func (s *Shard) insertPoint(txn *badger.Txn, startPoint ShardPoint, point ShardP
 			candidateSet.AddPoint(nn...)
 			candidateSet.AddPoint(point)
 			candidateSet.Sort()
-			s.robustPrune(txn, &n, candidateSet, s.collection.Parameters.Alpha, s.collection.Parameters.DegreeBound)
+			s.robustPrune(&n, candidateSet, s.collection.Parameters.Alpha, s.collection.Parameters.DegreeBound)
 		} else {
 			// ---------------------------
 			// Add the edge
 			n.Edges = append(n.Edges, point.Id)
 		}
-		if err := s.setPointEdges(txn, n); err != nil {
+		if err := s.setPointEdges(b, n); err != nil {
 			return fmt.Errorf("could not set neighbour point: %w", err)
 		}
 	}
 	// ---------------------------
-	if err := s.setPoint(txn, point); err != nil {
+	if err := s.setPoint(b, point); err != nil {
 		return fmt.Errorf("could not set point: %w", err)
 	}
 	// ---------------------------
@@ -105,25 +112,13 @@ func (s *Shard) UpsertPoints(points []models.Point) (map[uuid.UUID]error, error)
 	// This is to stop the shard from being closed while we're writing to it
 	s.changeLockCount(1)
 	// ---------------------------
-	// Does the points exist?
-	pointExists := make(map[uuid.UUID]struct{})
-	err := s.db.View(func(txn *badger.Txn) error {
-		for _, point := range points {
-			if _, err := txn.Get(point.Id[:]); err != nil {
-				pointExists[point.Id] = struct{}{}
-			}
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, fmt.Errorf("could not check if points exist: %w", err)
-	}
-	// ---------------------------
 	// Get start point
 	var startPoint ShardPoint
-	err = s.db.Update(func(txn *badger.Txn) error {
+	err := s.db.Update(func(tx *bbolt.Tx) error {
+		b := tx.Bucket([]byte("points"))
+		var err error
 		candidate := ShardPoint{Point: points[0]}
-		startPoint, err = s.getOrSetStartPoint(txn, candidate)
+		startPoint, err = s.getOrSetStartPoint(b, candidate)
 		return err
 	})
 	if err != nil {
@@ -133,19 +128,42 @@ func (s *Shard) UpsertPoints(points []models.Point) (map[uuid.UUID]error, error)
 	// Upsert points
 	results := make(map[uuid.UUID]error)
 	bar := progressbar.Default(int64(len(points)))
+	pointC := make(chan models.Point)
+	// Start workers
+	for i := 0; i < 8; i++ {
+		go func() {
+			for point := range pointC {
+				// ---------------------------
+				// Write point to shard
+				err := s.db.Batch(func(tx *bbolt.Tx) error {
+					b := tx.Bucket([]byte("points"))
+					return s.insertPoint(b, startPoint, ShardPoint{Point: point})
+				})
+				if err != nil {
+					log.Debug().Err(err).Msg("could not insert point")
+					results[point.Id] = err
+				}
+				bar.Add(1)
+				// ---------------------------
+			}
+		}()
+	}
 	for _, point := range points {
 		// ---------------------------
 		// Write point to shard
-		err := s.db.Update(func(txn *badger.Txn) error {
-			bar.Add(1)
-			return s.insertPoint(txn, startPoint, ShardPoint{Point: point})
-		})
-		if err != nil {
-			log.Debug().Err(err).Msg("could not insert point")
-			results[point.Id] = err
-		}
+		pointC <- point
+		// err := s.db.Update(func(tx *bbolt.Tx) error {
+		// 	bar.Add(1)
+		// 	b := tx.Bucket([]byte("points"))
+		// 	return s.insertPoint(b, startPoint, ShardPoint{Point: point})
+		// })
+		// if err != nil {
+		// 	log.Debug().Err(err).Msg("could not insert point")
+		// 	results[point.Id] = err
+		// }
 		// ---------------------------
 	}
+	close(pointC)
 	bar.Close()
 	// ---------------------------
 	return results, nil
@@ -157,9 +175,9 @@ func (s *Shard) SearchPoints(query []float32, k int) ([]models.Point, error) {
 	// ---------------------------
 	// Get start point
 	var startPoint ShardPoint
-	err := s.db.Update(func(txn *badger.Txn) error {
+	err := s.db.Update(func(tx *bbolt.Tx) error {
 		var err error
-		startPoint, err = s.getStartPoint(txn)
+		startPoint, err = s.getStartPoint(tx.Bucket([]byte("points")))
 		return err
 	})
 	if err != nil {
@@ -168,9 +186,10 @@ func (s *Shard) SearchPoints(query []float32, k int) ([]models.Point, error) {
 	// ---------------------------
 	// Perform search
 	var searchSet DistSet
-	err = s.db.View(func(txn *badger.Txn) error {
+	err = s.db.View(func(tx *bbolt.Tx) error {
 		var err error
-		searchSet, _, err = s.greedySearch(txn, startPoint, query, k, s.collection.Parameters.SearchSize)
+		b := tx.Bucket([]byte("points"))
+		searchSet, _, err = s.greedySearch(b, startPoint, query, k, s.collection.Parameters.SearchSize)
 		return err
 	})
 	if err != nil {
@@ -180,10 +199,11 @@ func (s *Shard) SearchPoints(query []float32, k int) ([]models.Point, error) {
 	// Clean up results and backfill metadata
 	searchSet.KeepFirstK(k)
 	results := make([]models.Point, len(searchSet.items))
-	err = s.db.View(func(txn *badger.Txn) error {
+	err = s.db.View(func(tx *bbolt.Tx) error {
+		b := tx.Bucket([]byte("points"))
 		for i, distElem := range searchSet.items {
 			results[i] = distElem.Point
-			timestamp, mdata, err := s.getPointTimestampMetadata(txn, distElem.Point.Id)
+			timestamp, mdata, err := s.getPointTimestampMetadata(b, distElem.Point.Id)
 			if err != nil {
 				return fmt.Errorf("could not get point timestamp, metadata: %w", err)
 			}
