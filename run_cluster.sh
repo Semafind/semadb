07835d04447d8c8c44729c087ec94b31b2bd1852
nuric <nuric@users.noreply.github.com>
2023-07-29 07:42:20 +0000

    Refactor server entry point to root level main

--- run_cluster.sh
+++ run_cluster.sh
@@ -6,13 +6,13 @@ export SEMADB_ROOT_DIR=./dump
 echo $SEMADB_SERVERS
 echo "Starting servers..."
 echo "Starting server 1"
-SEMADB_HTTP_PORT=8081 SEMADB_RPC_PORT=11001 go run ./server &
+SEMADB_HTTP_PORT=8081 SEMADB_RPC_PORT=11001 go run ./ &
 pid[0]=$!
 echo "Starting server 2"
-SEMADB_HTTP_PORT=8082 SEMADB_RPC_PORT=11002 go run ./server &
+SEMADB_HTTP_PORT=8082 SEMADB_RPC_PORT=11002 go run ./ &
 pid[1]=$!
 echo "Starting server 3"
-SEMADB_HTTP_PORT=8083 SEMADB_RPC_PORT=11003 go run ./server &
+SEMADB_HTTP_PORT=8083 SEMADB_RPC_PORT=11003 go run ./ &
 pid[2]=$!
 jobs
 trap "kill ${pid[0]} ${pid[1]} ${pid[2]}; exit 1" INT
