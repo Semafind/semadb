1cbe388d10dadeb9729a7f510740dbe91fc9ec81
nuric <nuric@users.noreply.github.com>
2023-09-20 13:50:09 +0000

    Add collection deletion test case

--- httpapi/httpapi_test.go
+++ httpapi/httpapi_test.go
@@ -197,32 +197,32 @@ func Test_GetCollection(t *testing.T) {
 	assert.Len(t, respBody.Shards, 0)
 }
 
-// func Test_DeleteCollection(t *testing.T) {
-// 	nodeS := ClusterNodeState{
-// 		Collections: []CollectionState{
-// 			{
-// 				Collection: models.Collection{
-// 					UserId:     "testy",
-// 					Id:         "gandalf",
-// 					VectorSize: 42,
-// 					DistMetric: "cosine",
-// 				},
-// 			},
-// 		},
-// 	}
-// 	router := setupTestRouter(t, nodeS)
-// 	// ---------------------------
-// 	// Unknown collection returns not found
-// 	resp := makeRequest(t, router, "DELETE", "/v1/collections/boromir", nil)
-// 	assert.Equal(t, http.StatusNotFound, resp.Code)
-// 	// ---------------------------
-// 	resp = makeRequest(t, router, "DELETE", "/v1/collections/gandalf", nil)
-// 	assert.Equal(t, http.StatusOK, resp.Code)
-// 	// ---------------------------
-// 	// Collection no longer exists
-// 	resp = makeRequest(t, router, "GET", "/v1/collections/gandalf", nil)
-// 	assert.Equal(t, http.StatusNotFound, resp.Code)
-// }
+func Test_DeleteCollection(t *testing.T) {
+	nodeS := ClusterNodeState{
+		Collections: []CollectionState{
+			{
+				Collection: models.Collection{
+					UserId:     "testy",
+					Id:         "gandalf",
+					VectorSize: 42,
+					DistMetric: "cosine",
+				},
+			},
+		},
+	}
+	router := setupTestRouter(t, nodeS)
+	// ---------------------------
+	// Unknown collection returns not found
+	resp := makeRequest(t, router, "DELETE", "/v1/collections/boromir", nil)
+	assert.Equal(t, http.StatusNotFound, resp.Code)
+	// ---------------------------
+	resp = makeRequest(t, router, "DELETE", "/v1/collections/gandalf", nil)
+	assert.Equal(t, http.StatusOK, resp.Code)
+	// ---------------------------
+	// Collection no longer exists
+	resp = makeRequest(t, router, "GET", "/v1/collections/gandalf", nil)
+	assert.Equal(t, http.StatusNotFound, resp.Code)
+}
 
 func Test_InsertPoints(t *testing.T) {
 	nodeS := ClusterNodeState{
