d19320e1db8bc607b9c7966eae4bf5e4ed4da3cb
nuric <nuric@users.noreply.github.com>
2023-12-15 14:49:19 +0000

    Add benchmark test for distance functions

--- distance/distance_test.go
+++ distance/distance_test.go
@@ -1,6 +1,8 @@
 package distance
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/semafind/semadb/distance/asm"
@@ -37,4 +39,39 @@ func TestASMSquaredEuclidean(t *testing.T) {
 	got := asm.SquaredEuclideanDistance(x, y)
 	want := float32(27)
 	assert.Equal(t, want, got)
+}
+
+var benchTable = []struct {
+	name string
+	fn   func([]float32, []float32) float32
+}{
+	{"PureDotProduct", dotProductPureGo},
+	{"ASMDotProduct", asm.Dot},
+	{"PureSquaredEuclidean", squaredEuclideanDistancePureGo},
+	{"ASMSquaredEuclidean", asm.SquaredEuclideanDistance},
+}
+
+var bechSizes = []int{768, 1536}
+
+func randVector(size int) []float32 {
+	vector := make([]float32, size)
+	for i := 0; i < size; i++ {
+		vector[i] = rand.Float32()
+	}
+	return vector
+}
+
+func BenchmarkDist(b *testing.B) {
+	for _, size := range bechSizes {
+		for _, bench := range benchTable {
+			x := randVector(size)
+			y := randVector(size)
+			runName := fmt.Sprintf("%s-%d", bench.name, size)
+			b.Run(runName, func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					bench.fn(x, y)
+				}
+			})
+		}
+	}
 }
