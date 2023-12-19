53156dcf3e6fe340a1c3bbaf8d07a625292518a4
nuric <nuric@users.noreply.github.com>
2023-12-19 16:16:49 +0000

    Add table driven distance tests
    Just covers a few more edge cases.

--- distance/distance_test.go
+++ distance/distance_test.go
@@ -9,28 +9,45 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var vectorTable = []struct {
+	name          string
+	x             []float32
+	y             []float32
+	wantDot       float32
+	wantEuclidean float32
+}{
+	{"Zero", []float32{0, 0, 0}, []float32{0, 0, 0}, 0, 0},
+	{"One", []float32{1, 1}, []float32{1, 1}, 2, 0},
+	{"Two", []float32{1, 2, 3}, []float32{4, 5, 6}, 32, 27},
+	{"Negative", []float32{-1, -2, -3}, []float32{-4, -5, -6}, 32, 27},
+	{"Mixed", []float32{-1, 2, 3}, []float32{4, -5, 6}, 4, 83},
+}
+
 func TestPureDotProduct(t *testing.T) {
-	x := []float32{1, 2, 3}
-	y := []float32{4, 5, 6}
-	got := dotProductPureGo(x, y)
-	want := float32(32)
-	assert.Equal(t, want, got)
+	for _, tt := range vectorTable {
+		t.Run(tt.name, func(t *testing.T) {
+			got := dotProductPureGo(tt.x, tt.y)
+			assert.Equal(t, tt.wantDot, got)
+		})
+	}
 }
 
 func TestASMdotProduct(t *testing.T) {
-	x := []float32{1, 2, 3}
-	y := []float32{4, 5, 6}
-	got := asm.Dot(x, y)
-	want := float32(32)
-	assert.Equal(t, want, got)
+	for _, tt := range vectorTable {
+		t.Run(tt.name, func(t *testing.T) {
+			got := asm.Dot(tt.x, tt.y)
+			assert.Equal(t, tt.wantDot, got)
+		})
+	}
 }
 
 func TestPureSquaredEuclidean(t *testing.T) {
-	x := []float32{1, 2, 3}
-	y := []float32{4, 5, 6}
-	got := squaredEuclideanDistancePureGo(x, y)
-	want := float32(27)
-	assert.Equal(t, want, got)
+	for _, tt := range vectorTable {
+		t.Run(tt.name, func(t *testing.T) {
+			got := squaredEuclideanDistancePureGo(tt.x, tt.y)
+			assert.Equal(t, tt.wantEuclidean, got)
+		})
+	}
 }
 
 func TestASMSquaredEuclidean(t *testing.T) {
