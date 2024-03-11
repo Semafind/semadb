643816d1a47bbdc0db15bd1549956df52aa599a5
nuric <nuric@users.noreply.github.com>
2024-03-11 11:47:25 +0000

    Update point and collection models to multi-index format

--- models/collection.go
+++ models/collection.go
@@ -1,16 +1,14 @@
 package models
 
+import "fmt"
+
 type Collection struct {
-	UserId     string
-	Id         string
-	VectorSize uint
-	DistMetric string
-	Replicas   uint
-	Algorithm  string
-	Timestamp  int64
-	CreatedAt  int64
-	ShardIds   []string
-	Parameters VamanaParameters
+	UserId    string
+	Id        string
+	Replicas  uint
+	Timestamp int64
+	CreatedAt int64
+	ShardIds  []string
 	// Active user plan
 	UserPlan    UserPlan
 	IndexSchema IndexSchema
@@ -18,7 +16,7 @@ type Collection struct {
 
 /* The IndexSchema is defined as different fields of index types to save us from
  * parsing arbitrary JSON. The original design was to include an object with
- * {type: indexType} and {parameters: indexParameters} fields. We quickly
+ * {type: indexType, parameters: indexParameters} fields. We quickly
  * realised this would create downstream parsing code. Although the explicit
  * approach may be verbose and may degrade user experience, it is far less error
  * prone due to strong typing. */
@@ -49,6 +47,113 @@ func (s *IndexSchema) CheckDuplicatePropValue() (string, bool) {
 	return "", false
 }
 
+type indexProperty struct {
+	Name string
+	Type string
+}
+
+func (s *IndexSchema) CollectAllProperties() []indexProperty {
+	var properties []indexProperty
+	// ---------------------------
+	// Do NOT change type values without handling migration. The type values are
+	// used in bucket names, e.g. index/vectorVamana/[property name]
+	// ---------------------------
+	for k := range s.VectorFlat {
+		properties = append(properties, indexProperty{Name: k, Type: "vectorFlat"})
+	}
+	for k := range s.VectorVamana {
+		properties = append(properties, indexProperty{Name: k, Type: "vectorVamana"})
+	}
+	for k := range s.Text {
+		properties = append(properties, indexProperty{Name: k, Type: "text"})
+	}
+	for k := range s.String {
+		properties = append(properties, indexProperty{Name: k, Type: "string"})
+	}
+	for k := range s.Integer {
+		properties = append(properties, indexProperty{Name: k, Type: "integer"})
+	}
+	for k := range s.Float {
+		properties = append(properties, indexProperty{Name: k, Type: "float"})
+	}
+	for k := range s.StringArray {
+		properties = append(properties, indexProperty{Name: k, Type: "stringArray"})
+	}
+	return properties
+}
+
+// Check if a given map is compatible with the index schema
+func (s *IndexSchema) CheckCompatibleMap(m map[string]any) error {
+	// We will go through each index field, check if the map has them, is of
+	// right type and any extra checks needed
+	// ---------------------------
+	for k, params := range s.VectorFlat {
+		if v, ok := m[k]; ok {
+			vector, ok := v.([]float32)
+			if !ok {
+				return fmt.Errorf("expected vector for %s, got %T", k, v)
+			}
+			if len(vector) != int(params.VectorSize) {
+				return fmt.Errorf("expected vector of size %d for %s, got %d", params.VectorSize, k, len(vector))
+			}
+		}
+	}
+	// ---------------------------
+	for k, params := range s.VectorVamana {
+		if v, ok := m[k]; ok {
+			vector, ok := v.([]float32)
+			if !ok {
+				return fmt.Errorf("expected vector for %s, got %T", k, v)
+			}
+			if len(vector) != int(params.VectorSize) {
+				return fmt.Errorf("expected vector of size %d for %s, got %d", params.VectorSize, k, len(vector))
+			}
+		}
+	}
+	// ---------------------------
+	for k := range s.Text {
+		if v, ok := m[k]; ok {
+			if _, ok := v.(string); !ok {
+				return fmt.Errorf("expected string for %s, got %T", k, v)
+			}
+		}
+	}
+	// ---------------------------
+	for k := range s.String {
+		if v, ok := m[k]; ok {
+			if _, ok := v.(string); !ok {
+				return fmt.Errorf("expected string for %s, got %T", k, v)
+			}
+		}
+	}
+	// ---------------------------
+	for k := range s.Integer {
+		if v, ok := m[k]; ok {
+			if _, ok := v.(int); !ok {
+				return fmt.Errorf("expected int for %s, got %T", k, v)
+			}
+		}
+	}
+	// ---------------------------
+	for k := range s.Float {
+		if v, ok := m[k]; ok {
+			if _, ok := v.(float32); !ok {
+				return fmt.Errorf("expected float for %s, got %T", k, v)
+			}
+		}
+	}
+	// ---------------------------
+	for k := range s.StringArray {
+		if v, ok := m[k]; ok {
+			if _, ok := v.([]string); !ok {
+				return fmt.Errorf("expected string array for %s, got %T", k, v)
+			}
+		}
+	}
+	// ---------------------------
+	return nil
+}
+
 type IndexVectorFlatParameters struct {
 	VectorSize     uint   `json:"vectorSize" binding:"required,min=1,max=4096"`
 	DistanceMetric string `json:"distanceMetric" binding:"required,oneof=euclidean cosine dot"`
@@ -65,20 +170,6 @@ type IndexTextParameters struct {
 	CaseSensitive bool `json:"caseSensitive"`
 }
 
-type VamanaParameters struct {
-	SearchSize  int
-	DegreeBound int
-	Alpha       float32
-}
-
 type IndexStringParameters struct {
 	CaseSensitive bool `json:"caseSensitive"`
-}
-
-func DefaultVamanaParameters() VamanaParameters {
-	return VamanaParameters{
-		SearchSize:  75,
-		DegreeBound: 64,
-		Alpha:       1.2,
-	}
 }
--- models/point.go
+++ models/point.go
@@ -1,9 +1,56 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"bytes"
+	"fmt"
 
+	"github.com/google/uuid"
+	"github.com/vmihailenco/msgpack/v5"
+)
+
+// This is the generic data type for a point that the user interacts with.
+type PointAsMap map[string]any
+
+// TODO: Refactor all marshalling business to this package
+
+func (p PointAsMap) ExtractIdField(createNew bool) (uuid.UUID, error) {
+	id, ok := p["_id"]
+	if !ok {
+		if createNew {
+			return uuid.New(), nil
+		}
+		return uuid.Nil, fmt.Errorf("missing _id field")
+	}
+	stringId, ok := id.(string)
+	if !ok {
+		return uuid.Nil, fmt.Errorf("invalid id type, expected string got %T", id)
+	}
+	parsedId, err := uuid.Parse(stringId)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("invalid id format, %s", err.Error())
+	}
+	// We remove the _id field because it is internal and not part of the
+	// actual point data. But to keep the API simpler, we combine the user
+	// request to include the _id field.
+	delete(p, "_id")
+	return parsedId, nil
+}
+
+// This is the internal data type for a point that the system uses. We extract
+// the id field for quick access and encode all the data into a byte slice.
 type Point struct {
-	Id       uuid.UUID
-	Vector   []float32
-	Metadata []byte
+	Id   uuid.UUID
+	Data []byte
+}
+
+func (p *Point) GetField(name string) (any, error) {
+	dec := msgpack.NewDecoder(bytes.NewReader(p.Data))
+	queryResult, err := dec.Query(name)
+	if err != nil {
+		return nil, fmt.Errorf("failed to query point field %s: %w", name, err)
+	}
+	if len(queryResult) == 0 {
+		return nil, fmt.Errorf("field %s not found in point", name)
+	}
+	return queryResult[0], nil
 }
