7db12ff6760e847f4794280110a0f43455a12093
nuric <nuric@users.noreply.github.com>
2024-03-22 17:31:00 +0000

    Add string array inverted index wrapper

--- shard/index/inverted/string.go
+++ shard/index/inverted/string.go
@@ -22,14 +22,6 @@ func NewIndexInvertedString(bucket diskstore.Bucket, params models.IndexStringPa
 	return &IndexInvertedString{inner: inv, params: params}, nil
 }
 
-// Transforms the data to lower case if the index is case insensitive
-func (inv *IndexInvertedString) preProcessValue(term string) string {
-	if !inv.params.CaseSensitive {
-		return strings.ToLower(term)
-	}
-	return term
-}
-
 func (inv *IndexInvertedString) InsertUpdateDelete(ctx context.Context, in <-chan IndexChange[string]) <-chan error {
 	// Process any transformers such as lowercasing before inserting
 	out := in
@@ -38,10 +30,10 @@ func (inv *IndexInvertedString) InsertUpdateDelete(ctx context.Context, in <-cha
 	if !inv.params.CaseSensitive {
 		out, transformErrC = utils.TransformWithContext(ctx, in, func(change IndexChange[string]) (IndexChange[string], bool, error) {
 			if change.CurrentData != nil {
-				*change.CurrentData = inv.preProcessValue(*change.CurrentData)
+				*change.CurrentData = strings.ToLower(*change.CurrentData)
 			}
 			if change.PreviousData != nil {
-				*change.PreviousData = inv.preProcessValue(*change.PreviousData)
+				*change.PreviousData = strings.ToLower(*change.PreviousData)
 			}
 			return change, false, nil
 		})
@@ -54,8 +46,49 @@ func (inv *IndexInvertedString) InsertUpdateDelete(ctx context.Context, in <-cha
 }
 
 func (inv *IndexInvertedString) Search(options models.SearchStringOptions) (*roaring64.Bitmap, error) {
-	query := inv.preProcessValue(options.Value)
+	query := options.Value
+	if !inv.params.CaseSensitive {
+		query = strings.ToLower(query)
+	}
 	return inv.inner.Search(query, options.EndValue, options.Operator)
 }
 
 // ---------------------------
+
+type IndexInvertedArrayString struct {
+	inner  *indexInvertedArray[string]
+	params models.IndexStringArrayParameters
+}
+
+func NewIndexInvertedArrayString(bucket diskstore.Bucket, params models.IndexStringArrayParameters) (*IndexInvertedArrayString, error) {
+	inv := NewIndexInvertedArray[string](bucket)
+	return &IndexInvertedArrayString{inner: inv, params: params}, nil
+}
+
+func (inv *IndexInvertedArrayString) InsertUpdateDelete(ctx context.Context, in <-chan IndexArrayChange[string]) <-chan error {
+	// Process any transformers such as lowercasing before inserting
+	out := in
+	// Do we need to pre process?
+	if !inv.params.CaseSensitive {
+		out, _ = utils.TransformWithContext(ctx, in, func(change IndexArrayChange[string]) (IndexArrayChange[string], bool, error) {
+			for i := range change.CurrentData {
+				change.CurrentData[i] = strings.ToLower(change.CurrentData[i])
+			}
+			for i := range change.PreviousData {
+				change.PreviousData[i] = strings.ToLower(change.PreviousData[i])
+			}
+			return change, false, nil
+		})
+	}
+	return inv.inner.InsertUpdateDelete(ctx, out)
+}
+
+func (inv *IndexInvertedArrayString) Search(options models.SearchStringArrayOptions) (*roaring64.Bitmap, error) {
+	query := options.Value
+	if !inv.params.CaseSensitive {
+		for i := range query {
+			query[i] = strings.ToLower(query[i])
+		}
+	}
+	return inv.inner.Search(query, options.Operator)
+}
