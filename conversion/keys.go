fa29dd261924c079d3ead86c050cf30cb561daf5
nuric <nuric@users.noreply.github.com>
2024-04-17 14:47:23 +0100

    Add comments for conversion keys file

--- conversion/keys.go
+++ conversion/keys.go
@@ -2,6 +2,7 @@ package conversion
 
 import "encoding/binary"
 
+// Converts a node id and a suffix to a byte slice key to be used in diskstore.
 func NodeKey(id uint64, suffix byte) []byte {
 	key := [10]byte{}
 	key[0] = 'n'
@@ -10,6 +11,7 @@ func NodeKey(id uint64, suffix byte) []byte {
 	return key[:]
 }
 
+// Checks if a given key and suffix is a valid a node key and returns the id.
 func NodeIdFromKey(key []byte, suffix byte) (uint64, bool) {
 	if len(key) != 10 || key[0] != 'n' || key[9] != suffix {
 		return 0, false
