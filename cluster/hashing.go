924dc8c2ae4f8243e636d30d58e62c0bd29537ad
nuric <nuric@users.noreply.github.com>
2024-08-14 17:47:51 +0100

    Refactor unused export in cluster hashing
    This is prep for cluster sync workload.

--- cluster/hashing.go
+++ cluster/hashing.go
@@ -7,13 +7,14 @@ import (
 	"github.com/cespare/xxhash"
 )
 
-type ServerScore struct {
-	Server string
-	Score  uint64
-}
-
 // RendezvousHash returns a list of servers sorted by their score for the given key.
 func RendezvousHash(key string, servers []string, topK int) []string {
+	// ---------------------------
+	type ServerScore struct {
+		Server string
+		Score  uint64
+	}
+	// ---------------------------
 	scores := make([]ServerScore, len(servers))
 	for i, server := range servers {
 		// combinedKey := append(key, []byte(server)...)
@@ -25,12 +26,11 @@ func RendezvousHash(key string, servers []string, topK int) []string {
 		return cmp.Compare(a.Score, b.Score)
 	})
 	// Convert back to string slice
-	resSize := len(servers)
-	if topK < resSize {
-		resSize = topK
+	if topK > len(servers) {
+		topK = len(servers)
 	}
-	res := make([]string, resSize)
-	for i := 0; i < resSize; i++ {
+	res := make([]string, topK)
+	for i := 0; i < topK; i++ {
 		res[i] = scores[i].Server
 	}
 	return res
