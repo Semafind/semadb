5d5a61c98d38bd6c05333e13e2ba9fba57b6d6a7
nuric <nuric@users.noreply.github.com>
2024-11-30 16:57:15 +0000

    Add validation to models for new standard lib httpapi
    This completes the first phase of migration to standard library, all functionality should be the same.

--- models/index.go
+++ models/index.go
@@ -10,39 +10,9 @@ import (
 type IndexSchema map[string]IndexSchemaValue
 
 func (s IndexSchema) Validate() error {
-	for k, v := range s {
-		switch v.Type {
-		case IndexTypeVectorFlat:
-			if v.VectorFlat == nil {
-				return fmt.Errorf("vectorFlat parameters not provided for property %s", k)
-			}
-			if v.VectorFlat.DistanceMetric == DistanceHaversine && v.VectorFlat.VectorSize != 2 {
-				return fmt.Errorf("haversine distance metric requires vector size 2 for property %s, got %d", k, v.VectorFlat.VectorSize)
-			}
-		case IndexTypeVectorVamana:
-			if v.VectorVamana == nil {
-				return fmt.Errorf("vectorVamana parameters not provided for property %s", k)
-			}
-			if v.VectorVamana.DistanceMetric == DistanceHaversine && v.VectorVamana.VectorSize != 2 {
-				return fmt.Errorf("haversine distance metric requires vector size 2 for property %s, got %d", k, v.VectorVamana.VectorSize)
-			}
-		case IndexTypeText:
-			if v.Text == nil {
-				return fmt.Errorf("text parameters not provided for property %s", k)
-			}
-		case IndexTypeString:
-			if v.String == nil {
-				return fmt.Errorf("string parameters not provided for property %s", k)
-			}
-		case IndexTypeStringArray:
-			if v.StringArray == nil {
-				return fmt.Errorf("stringArray parameters not provided for property %s", k)
-			}
-		case IndexTypeInteger:
-			// Nothing to check
-		case IndexTypeFloat:
-		default:
-			return fmt.Errorf("unknown index type %s", v.Type)
+	for _, v := range s {
+		if err := v.Validate(); err != nil {
+			return err
 		}
 	}
 	return nil
@@ -55,6 +25,51 @@ type IndexSchemaValue struct {
 	Text         *IndexTextParameters         `json:"text,omitempty"`
 	String       *IndexStringParameters       `json:"string,omitempty"`
 	StringArray  *IndexStringArrayParameters  `json:"stringArray,omitempty"`
+}
+
+func (v IndexSchemaValue) Validate() error {
+	if v.Type != IndexTypeVectorFlat &&
+		v.Type != IndexTypeVectorVamana &&
+		v.Type != IndexTypeText &&
+		v.Type != IndexTypeString &&
+		v.Type != IndexTypeInteger &&
+		v.Type != IndexTypeFloat &&
+		v.Type != IndexTypeStringArray {
+		return fmt.Errorf("unknown index type %s", v.Type)
+	}
+	switch v.Type {
+	case IndexTypeVectorFlat:
+		if v.VectorFlat == nil {
+			return fmt.Errorf("vectorFlat parameters not provided for type %s", v.Type)
+		}
+		return v.VectorFlat.Validate()
+	case IndexTypeVectorVamana:
+		if v.VectorVamana == nil {
+			return fmt.Errorf("vectorVamana parameters not provided for type %s", v.Type)
+		}
+		return v.VectorVamana.Validate()
+	case IndexTypeText:
+		if v.Text == nil {
+			return fmt.Errorf("text parameters not provided for type %s", v.Type)
+		}
+		return v.Text.Validate()
+	case IndexTypeString:
+		if v.String == nil {
+			return fmt.Errorf("string parameters not provided for type %s", v.Type)
+		}
+		return v.String.Validate()
+	case IndexTypeStringArray:
+		if v.StringArray == nil {
+			return fmt.Errorf("stringArray parameters not provided for type %s", v.Type)
+		}
+		return v.StringArray.Validate()
+	case IndexTypeInteger:
+		// Nothing to check
+	case IndexTypeFloat:
+	default:
+		return fmt.Errorf("unknown index type %s", v.Type)
+	}
+	return nil
 }
 
 // Attempts to convert a given value to a vector
@@ -236,6 +251,27 @@ type IndexVectorFlatParameters struct {
 	Quantizer      *Quantizer `json:"quantizer,omitempty"`
 }
 
+func (p IndexVectorFlatParameters) Validate() error {
+	if p.VectorSize < 1 || p.VectorSize > 4096 {
+		return fmt.Errorf("vector size must be between 1 and 4096, got %d", p.VectorSize)
+	}
+	if p.DistanceMetric != DistanceEuclidean &&
+		p.DistanceMetric != DistanceCosine &&
+		p.DistanceMetric != DistanceDot &&
+		p.DistanceMetric != DistanceHamming &&
+		p.DistanceMetric != DistanceJaccard &&
+		p.DistanceMetric != DistanceHaversine {
+		return fmt.Errorf("unknown distance metric %s", p.DistanceMetric)
+	}
+	if p.DistanceMetric == DistanceHaversine && p.VectorSize != 2 {
+		return fmt.Errorf("haversine distance metric requires vector size 2 got %d", p.VectorSize)
+	}
+	if p.Quantizer != nil {
+		return p.Quantizer.Validate()
+	}
+	return nil
+}
+
 type IndexVectorVamanaParameters struct {
 	VectorSize     uint       `json:"vectorSize" binding:"required,min=1,max=4096"`
 	DistanceMetric string     `json:"distanceMetric" binding:"required,oneof=euclidean cosine dot hamming jaccard haversine"`
@@ -245,14 +281,60 @@ type IndexVectorVamanaParameters struct {
 	Quantizer      *Quantizer `json:"quantizer,omitempty"`
 }
 
+func (p IndexVectorVamanaParameters) Validate() error {
+	if p.VectorSize < 1 || p.VectorSize > 4096 {
+		return fmt.Errorf("vector size must be between 1 and 4096, got %d", p.VectorSize)
+	}
+	if p.DistanceMetric != DistanceEuclidean &&
+		p.DistanceMetric != DistanceCosine &&
+		p.DistanceMetric != DistanceDot &&
+		p.DistanceMetric != DistanceHamming &&
+		p.DistanceMetric != DistanceJaccard &&
+		p.DistanceMetric != DistanceHaversine {
+		return fmt.Errorf("unknown distance metric %s", p.DistanceMetric)
+	}
+	if p.DistanceMetric == DistanceHaversine && p.VectorSize != 2 {
+		return fmt.Errorf("haversine distance metric requires vector size 2 got %d", p.VectorSize)
+	}
+	if p.SearchSize < 25 || p.SearchSize > 75 {
+		return fmt.Errorf("search size must be between 25 and 75, got %d", p.SearchSize)
+	}
+	if p.DegreeBound < 32 || p.DegreeBound > 64 {
+		return fmt.Errorf("degree bound must be between 32 and 64, got %d", p.DegreeBound)
+	}
+	if p.Alpha < 1.1 || p.Alpha > 1.5 {
+		return fmt.Errorf("alpha must be between 1.1 and 1.5, got %f", p.Alpha)
+	}
+	if p.Quantizer != nil {
+		return p.Quantizer.Validate()
+	}
+	return nil
+
+}
+
 type IndexTextParameters struct {
 	Analyser string `json:"analyser" binding:"required,oneof=standard"`
+}
+
+func (p IndexTextParameters) Validate() error {
+	if p.Analyser != "standard" {
+		return fmt.Errorf("unknown analyser %s", p.Analyser)
+	}
+	return nil
 }
 
 type IndexStringParameters struct {
 	CaseSensitive bool `json:"caseSensitive"`
 }
 
+func (p IndexStringParameters) Validate() error {
+	return nil
+}
+
 type IndexStringArrayParameters struct {
 	IndexStringParameters
+}
+
+func (p IndexStringArrayParameters) Validate() error {
+	return p.IndexStringParameters.Validate()
 }
